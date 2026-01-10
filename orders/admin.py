ed2c3ee02e98833649ad079d17b20d9d2cd2503a
j-victor29 <jv4545666@gmail.com>
2026-01-10 01:40:13 -0300

    registra modelos Pedido e ItemPedido no admin; configura exibição, filtros e campos de busca

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pedido, ItemPedido
 
 # Register your models here.
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'status_pedido', 'valor_total', 'data_criacao')
+    list_filter = ('status_pedido', 'data_criacao')
+    search_fields = ('usuario__username',)
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pedido', 'produto', 'quantidade', 'preco_unitario', 'subtotal')
+    search_fields = ('pedido__id', 'produto__nome')
+    list_filter = ('produto',)
